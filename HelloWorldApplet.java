f84dda9a89d29d90d0c49b643e4dfc3968670475
Tom Xue <tomxue026@gmail.com>
2012-04-02 23:27:49 +0800

    to show how to make a simple java applet works

---
+++ HelloWorldApplet.java
@@ -0,0 +1,8 @@
+import java.awt.*;
+import java.applet.*;
+
+public class HelloWorldApplet extends Applet { //an applet
+	public void paint(Graphics g){
+		g.drawString ("Hello World!",20,20);
+		}
+} 
